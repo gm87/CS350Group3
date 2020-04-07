9eeb38e633f286444e35479650f846008ab669d5
Graham Matthews <gm87@evansville.edu>
2020-04-06 20:48:35 -0500

    basic layout

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<!-- CS350 Group 3
+
+-->
+
+<html lang="en">
+    <head>
+        <title>TITLE HERE</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,
+                    initial-scale=1.0">
+        <link rel="stylesheet" href="styles.css" />
+    </head>
+
+    <body>
+        <div class="pagewrapper">
+            <nav> <!-- navbar -->
+                <ul>
+                    <li id="tab1">Home</li>
+                    <li id="tab2">About</li>
+                    <li id="tab3">Forum</li>
+                </ul>
+            </nav> <!-- end navbar -->
+            <section id="page1">
+                <h1>Page 1</h1>
+            </section> <!-- end page 1 -->
+            <section id="page2">
+                <h1>Page 2</h1>
+            </section> <!-- end page 2 -->
+            <section id="page3">
+                <h1>Page 3</h1>
+            </section> <!-- end page 3 -->
+        </div> <!-- end pagewrapper div -->
+    </body>
+</html>
