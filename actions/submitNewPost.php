fa14c75910845ecb27e5825c81b7ce88a58b5612
ks562 <ks562@csserver.evansville.edu>
2020-04-24 23:51:39 -0500

    added submitNewPost

--- actions/submitNewPost.php
+++ actions/submitNewPost.php
@@ -5,4 +5,5 @@
     $body = $_POST['body'];
     file_put_contents("../messages/" . $time . "/originalPost.json", 
     "{\n\t\"title\": \"" . $title . "\", \n\t\"body\": \"" . $body . "\" \n}");
+    http_response_code(200);
 ?>
